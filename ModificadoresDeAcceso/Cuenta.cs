8e9c13456012a107ddcaab3617878cd04121261a
alumno <alumno@E326>
2019-08-13 07:50:06 -0700

    Okay

---
+++ ModificadoresDeAcceso/Cuenta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresDeAcceso
+{
+    class Cuenta
+    {
+
+        public Cliente Propietario { get; set; }
+        public string _identificador { get; set;}
+        public string identificador { get; set;}
+        public float saldo { get; set; }
+
+        public void retirar ()
+        {
+
+        }
+
+        public void Depositar()
+        {
+
+        }
+    }
+}
